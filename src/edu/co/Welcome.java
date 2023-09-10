2b889748ae3cc69d46dbc83c67ab220222c24429
edureka <abhardwaj273@gmail.com>
2023-09-10 11:02:44 +0530

    first commit

---
+++ src/edu/co/Welcome.java
@@ -0,0 +1,18 @@
+package edu.co;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld");
+		System.out.println("Welcome to Edureka");
+		System.out.println("Welcome to Continous testing batch");
+		System.out.println("Welcome to Data Science");
+		System.out.println("Welcome to IOT");
+		System.out.println("Welcome to cloud computing");
+		System.out.println("Welcome to Artificil Intelligence");
+		System.out.println("Welcome to Data Analytics");
+
+	}
+
+}
